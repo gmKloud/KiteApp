c14ac62e2dd4c0a5736cc00a703ccb24152cac56
Jiwon Kang and grinil khanna <jiwon+grinil@>
2017-05-11 12:03:11 -0700

    Add JObject and API info template in Location.cs models - callign API yet still need to input correct info

---
+++ src/KiteApp/Models/Location.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace KiteApp.Models
+{
+    public class Location
+    {
+        public int Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string CountryCode { get; set; }
+
+        public static List<Location> GetLocations()
+        {
+            var client = new RestClient("httttpp"); // fill in correct http
+            var request = new RestRequest("request of site and how it works", Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator("{{Account SID}}", "{{Auth Token}}"); //Fill these in with weather APp info
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<Object>(response.Content);
+            var messageList = JsonConvert.DeserializeObject<List<Location>>(jsonResponse["locations"].ToString()); //check if called locations
+            return locationList;
+        }
+
+        public void Send()
+        {
+            var client = new RestClient("hhhttttppp");
+            var request = new RestRequest("Account//{{Account SSID}}/Locations", Method.POST); // Fill these in with weather APp info
+            request.AddParameter(); //choose parameter
+            request.AddParameter(); //choose parameter
+            request.AddParameter(); //choose parameter
+            client.Authenticator = new HttpBasicAuthenticator("{{Account SSID}}", "{{Auth Token}}");
+            client.ExecuteAsync(request, response =>
+            {
+                Console.WriteLine(response.Content);
+            });
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
